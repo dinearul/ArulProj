a44fb7521adff387e7de700aba26913fa10ce828
Sundar <sundaravadive.chandran@cognizant.com>
2019-11-19 10:26:47 +0530

    first commit

---
+++ sample/src/main/java/com/klarthik/sample/NalController.java
@@ -0,0 +1,18 @@
+package com.klarthik.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/nal")
+public class NalController {
+
+    @GetMapping("per")
+    public String per(@RequestParam("name") String peru, @RequestParam int age) {
+
+        String d = "Hello " + peru + " And age is " + age;
+        return d;
+    }
+}
